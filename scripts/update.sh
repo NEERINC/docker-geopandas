2007225af13c1951fd8c5b83bf81bffc111afb72
Matthew Downs <matthew@neer.ai>
2022-03-17 10:52:33 -0500

    Uncomment

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-#echo
-#echo "***** WARNING *****"
-#echo
-#echo "The *-alpine variant of this image can take up to 30 minutes to build due to no pre-compiled binaries existing in Alpine."
-#echo "If you want to update a non-Alpine tag, it is recommended to build it individually."
-#echo
-#echo "Continue anyways?"
-#read -p "Press any key to continue, or Ctrl+C to cancel "
+echo
+echo "***** WARNING *****"
+echo
+echo "The *-alpine variant of this image can take up to 30 minutes to build due to no pre-compiled binaries existing in Alpine."
+echo "If you want to update a non-Alpine tag, it is recommended to build it individually."
+echo
+echo "Continue anyways?"
+read -p "Press any key to continue, or Ctrl+C to cancel "
 
 BASE=$(dirname "$0")
 
 # 0.9.0
-#docker push $(VERSION=0.9.0 VARIANT=alpine ${BASE}/build.sh)
+docker push $(VERSION=0.9.0 VARIANT=alpine ${BASE}/build.sh)
 docker push $(VERSION=0.9.0 VARIANT=bullseye ${BASE}/build.sh)
 docker push $(VERSION=0.9.0 VARIANT=buster ${BASE}/build.sh)
